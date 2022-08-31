976d0cee088347fddfb19d08c73d81246833779d
Nathankaleng <nathankaleng@gmail.com>
2022-08-31 16:26:43 +0300

    Deleted the services

---
+++ TransportApp.Application/Repositories/IBusStopRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransportApp.Domain;
+
+namespace TransportAppApplication.Repositories
+{
+    public interface IBusStopRepository
+    {
+        Task<BusStop> CreateNewBusStopAsync(BusStop street);
+
+        Task<List<BusStop>> GetListBusStopAsync();
+
+        Task<BusStop> UpdateBusStopAsync(int id);
+
+        Task<BusStop> DeleteBusStopAsync(int id);
+    }
+}
---
+++ TransportApp.Application/Repositories/IRouteRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransportApp.Domain;
+
+namespace TransportAppApplication.Repositories
+{
+    public interface IRouteRepository
+    {
+        Task<Route> CreateNewRouteAsync(Route street);
+
+        Task<List<Route>> GetListRouteAsync();
+
+        Task<Route> UpdateRouteAsync(int id);
+
+        Task<Route> DeleteRouteAsync(int id);
+    }
+}
